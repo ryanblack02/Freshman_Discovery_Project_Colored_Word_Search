83b030627f4fe22e95a732c682df40eb8f75bb6f
Ryan Black <rblack02@byu.edu>
2025-10-02 10:02:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// Example: Just a small sample to test
+const words = ["Dog", "Cat", "Horse", "Elephant", "Tiger"];
 
+const container = document.getElementById("word-container");
+
+// Display words
+container.innerHTML = words.join(", ");
