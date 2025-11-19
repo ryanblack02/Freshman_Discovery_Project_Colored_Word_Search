cb821d1a23cb173c542c62a3cd6f2d24d2e34772
Ryan Black <rblack02@byu.edu>
2025-11-18 22:35:08 -0700

    Expand animal database with additional species

--- script.js
+++ script.js
@@ -20,46 +20,140 @@ const neonClassForIndex = i => `highlight-${neonNames[i % neonNames.length]}`;
 const randomChoice = arr => arr[Math.floor(Math.random() * arr.length)];
 const randInt = max => Math.floor(Math.random() * max);
 
-/* --- Word database --- */
 const words = {
   mammals: {
-    kid: ["Dog","Cat","Cow","Horse","Pig","Sheep","Goat","Rabbit","Lion","Tiger",
-          "Elephant","Bear","Monkey","Giraffe","Kangaroo","Panda","Zebra","Deer",
-          "Fox","Wolf","Dolphin","Whale","Seal","Otter","Squirrel","Mouse","Rat",
-          "Hedgehog","Bat","Raccoon"],
-    adult: ["Aardvark","Aardwolf","Alpaca","Antelope","Bison","Capybara","Chimpanzee",
-            "Cheetah","Dugong","Echidna","Fennecfox","Gerenuk","Hartebeest","Hyena",
-            "Impala","Jackal","Koala","Lemur","Manatee","Meerkat","Moose","Narwhal",
-            "Okapi","Platypus","Porcupine","Redpanda","Sloth","Tapir","Warthog",
-            "Wolverine"]
+    kid: [
+      "Dog","Cat","Cow","Horse","Pig","Sheep","Goat","Rabbit","Lion","Tiger",
+      "Elephant","Bear","Monkey","Giraffe","Kangaroo","Panda","Zebra","Deer",
+      "Fox","Wolf","Dolphin","Whale","Seal","Otter","Squirrel"
+    ],
+    adult: [
+      "Aardvark","Aardwolf","Alpaca","Antelope","Baboon","Badger","Bison","Bobcat",
+      "Capybara","Caribou","Civet","Coyote","Dugong","Eland","Echidna","Ermine",
+      "FennecFox","Ferret","Gazelle","Gerenuk","Gibbon","Goral","Groundhog","Hedgehog",
+      "Hippopotamus","Hyaena","Ibex","Impala","Jackal","Jaguar","Kakapo","KangarooRat",
+      "Kinkajou","Koala","Kudu","Lemur","Lynx","Manatee","Marten","Meerkat",
+      "Mongoose","Moose","Muskox","Narwhal","Numbat","Ocelot","Okapi","Orangutan",
+      "OtterSea","Pangolin","Peccary","Pika","Platypus","Porcupine","PrairieDog",
+      "ProboscisMonkey","Pronghorn","Puma","Quokka","Quoll","Raccoon","Reindeer",
+      "RedPanda","RestlessOne","Ringtail","SeaLion","Serval","Shearwater","Shrew",
+      "SlowLoris","SpermWhale","SableAntelope","Sifaka","Skunk","Sloth","Stoat",
+      "Tapir","TasmanianDevil","Tamarin","TigerShark","Vervet","Vicuna","Warthog",
+      "WaterBuffalo","Weasel","Wolverine","Yak","Zorilla"
+    ]
   },
+
   birds: {
-    kid: ["Chicken","Duck","Goose","Swan","Owl","Eagle","Hawk","Parrot","Penguin",
-          "Flamingo","Peacock","Crow","Sparrow","Robin","Hummingbird","Toucan",
-          "Woodpecker","Seagull","Pelican","Ostrich"],
-    adult: ["Albatross","Cassowary","Cockatoo","Condor","Crane","Falcon","Heron",
-            "Hornbill","Kingfisher","Kookaburra","Macaw","Nightingale","Quail",
-            "Roadrunner","Shoebill"]
+    kid: [
+      "Chicken","Duck","Goose","Swan","Owl","Eagle","Hawk","Parrot","Penguin","Flamingo",
+      "Peacock","Crow","Sparrow","Robin","Hummingbird","Toucan","Woodpecker","Seagull",
+      "Pelican","Ostrich","Dove","Pigeon","Turkey","Condor","Stork"
+    ],
+    adult: [
+      "Albatross","Anhinga","Avocet","BaldEagle","BarnOwl","BarnSwallow","Bittern",
+      "Bowerbird","Budgerigar","Buzzard","Cacique","Canary","Cassowary","Cockatoo",
+      "Cormorant","Crane","Cuckoo","Curlew","DoveTanager","Dovekie","Drongo",
+      "Dunlin","Eider","Egret","Emu","Falcon","Firefinch","Flycatcher","Gannet",
+      "Goldfinch","Goshawk","Grebe","Grossbeak","Grouse","Guineafowl","Gull",
+      "Harrier","Heron","Honeycreeper","Hoopoe","Hornbill","Ibis","Jacana","Junco",
+      "Kestrel","Ketupa","Kingfisher","Kite","Kookaburra","Lapwing","Lark",
+      "Lyrebird","Magpie","Marabou","Myna","Nightingale","Nuthatch","Osprey",
+      "Oystercatcher","Parakeet","Parrotlet","Petrel","Plover","Ptarmigan",
+      "Quail","Rail","Raven","Roadrunner","RosyFinch","Rook","Sandpiper","Spoonbill",
+      "Stilt","Sunbird","SwanGoose","Tern","Turnstone","Vireo","Vulture","Warbler",
+      "Waxwing","Wryneck"
+    ]
   },
+
   reptiles: {
-    kid: ["Snake","Lizard","Turtle","Crocodile","Alligator","Chameleon","Gecko","Iguana"],
-    adult: ["Komododragon","GarterSnake","Boa","Python","Anole","Tortoise","Monitor","Basilisk","Skink","Viper"]
+    kid: [
+      "Snake","Lizard","Turtle","Crocodile","Alligator","Chameleon","Gecko","Iguana",
+      "Tortoise","Skink","Anole","GarterSnake","Copperhead"
+    ],
+    adult: [
+      "KomodoDragon","Boa","Python","Monitor","Basilisk","Viper","Mamba","Taipan",
+      "GilaMonster","Rattlesnake","Copperhead","CoralSnake","KingCobra","WaterPython",
+      "GreenAnaconda","GlassLizard","HornedLizard","BeardedDragon","Adder","LeopardTortoise",
+      "GalapagosTortoise","DesertTortoise","AmericanAlligator","SaltwaterCrocodile",
+      "SpectacledCaiman","CrocodileMonitor","FlyingDragon","SeaSnake","BandedKrait",
+      "BlackMamba","BushSnake","Coachwhip","CopperbellySalamander","Diamondback",
+      "EasternTigerSnake","FenceLizard","GarterSnake","HornedViper","IndianPython",
+      "JavelinLizard","KingRatSnake","LeafNosedGecko","MexicanBeadedLizard",
+      "NightLizard","OliveRidley","PlainsGarter","QuinceMonitor","RedTailedBoa",
+      "Scheltopusik","TreeBoa","Uromastyx","ViperBoa","WaterMonitor","YellowAnaconda",
+      "Zebra-tailedLizard"
+    ]
   },
+
   amphibians: {
-    kid: ["Frog","Toad","Salamander","Newt","TreeFrog","Bullfrog","Mudpuppy","Tadpole"],
-    adult: ["Axolotl","Caecilian","Hellbender","Sirens","Olm","TigerSalamander","FireSalamander","GiantSalamander"]
+    kid: [
+      "Frog","Toad","Salamander","Newt","TreeFrog","Bullfrog","Mudpuppy","Tadpole",
+      "Axolotl","GlassFrog"
+    ],
+    adult: [
+      "Caecilian","Hellbender","Sirens","Olm","TigerSalamander","FireSalamander",
+      "GiantSalamander","PoisonDartFrog","GlassFrogSpecies","MarshFrog","PickerelFrog",
+      "RedEft","SpadefootToad","WoodFrog","DarwinFrog","CommonToad","EuropeanNewt",
+      "OliveToad","CaneToad","Salamandra","GiantClawedFrog","SurinamToad","HoodedFrog",
+      "AfricanClawedFrog","BorealToad","NatterjackToad","MoorFrog","TomatoFrog",
+      "FireBelliedToad","MidwifeToad","SmoothNewt","YellowBelliedToad","EasternNewt",
+      "Ambystoma","TigerSalamanderSp"
+    ]
   },
+
   fish: {
-    kid: ["Goldfish","Shark","Tuna","Trout","Salmon","Clownfish","Catfish","MantaRay","Angelfish","Guppy"],
-    adult: ["Barracuda","Grouper","Swordfish","Anglerfish","Lionfish","Betta","Piranha","Marlin","Sturgeon","Snapper"]
+    kid: [
+      "Goldfish","Shark","Tuna","Trout","Salmon","Clownfish","Catfish","MantaRay",
+      "Angelfish","Guppy","Seahorse","Stingray","Carp","Pufferfish","Perch"
+    ],
+    adult: [
+      "Barracuda","Grouper","Swordfish","Anglerfish","Lionfish","Betta","Piranha",
+      "Marlin","Sturgeon","Snapper","Halibut","Mackerel","Wahoo","Tarpon","Coelacanth",
+      "ElectricEel","BluefinTuna","AtlanticCod","PacificHerring","Yellowtail",
+      "Kingfish","Bonefish","Archerfish","Bichir","Blowfish","BroadaxeGurnard",
+      "Butterfish","Carp","ChannelCatfish","Cusk","Dogfish","Dorado","Dory",
+      "Eelpout","Flounder","Garfish","Goby","Hake","Herring","JackCrevalle","JohnDory",
+      "Lamprey","Lizardfish","MahiMahi","Monkfish","MorayEel","Needlefish",
+      "NorthernPike","OrangeRoughy","Parrotfish","Pilotfish","Pollock","Pomfret",
+      "Pompano","Puffer","RibbonFish","Sailfish","Sculpin","Shad","Skate","Smelt",
+      "Sole","Sunfish","Tilapia","Toadfish","Triggerfish","Weakfish","Whitefish",
+      "Wrasse","YellowfinTuna"
+    ]
   },
+
   insects: {
-    kid: ["Ant","Bee","Butterfly","Beetle","Fly","Wasp","Grasshopper","Ladybug","Firefly","Caterpillar"],
-    adult: ["Dragonfly","Cricket","Termite","Moth","Mosquito","PrayingMantis","Cockroach","Hornet","Locust","Earwig"]
+    kid: [
+      "Ant","Bee","Butterfly","Beetle","Fly","Wasp","Grasshopper","Ladybug","Firefly",
+      "Caterpillar","Dragonfly","Moth","Cricket","Honeybee","Bumblebee"
+    ],
+    adult: [
+      "Aphid","AssassinBug","AtlasMoth","BarkBeetle","BeeEater","BombardierBeetle",
+      "BoxElderBug","CarpenterAnt","Cicada","ClickBeetle","Cockroach","Cranefly",
+      "DamselFly","Dobsonfly","Earwig","Flea","FruitFly","GiantWaterBug","Gnat",
+      "GrasshopperSp","GreenJuneBeetle","GroundBeetle","Hopper","Ichneumon",
+      "JuneBug","Katydid","LeafInsect","LeafcutterAnt","Locust","Louse","Mantid",
+      "Mayfly","MoleCricket","NetWing","QueenBee","RoveBeetle","Sawfly","ScorpionFly",
+      "Silkworm","SoldierBeetle","StinkBug","Stonefly","Termite","Thrips","TigerBeetle",
+      "Treehopper","Vespid","Weevil","Whirligig","Yellowjacket","Zoraptera"
+    ]
   },
+
   invertebrates: {
-    kid: ["Snail","Worm","Jellyfish","Crab","Octopus","Starfish","Clam","Shrimp","SeaUrchin","Sponge"],
-    adult: ["Lobster","Scorpion","Coral","Anemone","Cuttlefish","Nautilus","Squid","Barnacle","SeaCucumber","Krill"]
+    kid: [
+      "Snail","Worm","Jellyfish","Crab","Octopus","Starfish","Clam","Shrimp","SeaUrchin",
+      "Sponge","Lobster","Slug","Scallop"
+    ],
+    adult: [
+      "Abalone","Anemone","Barnacle","BrittleStar","Buccinoid",
+      "Cuttlefish","Cymothoid","Cymothoa","CrownOfThorns","Ctenophore","DungenessCrab",
+      "FeatherDusterWorm","Flatworm","GiantClam","GhostCrab","Gorgonian","HermitCrab",
+      "HorseshoeCrab","Isopod","Krill","Leech","LionManeJellyfish","Limpet","LobsterSp",
+      "MantisShrimp","MoonJelly","Nautilus","Octocoral","PenaeidPrawn","PearlOyster",
+      "PortugueseManOWar","SeaAnemone","SeaCucumber","SeaFan","SeaSlater","SeaSquirt",
+      "SeaWasp","ShrimpCleaner","Siphonophore","SpinyLobster","SquatLobster","Squid",
+      "TarantulaSeaAnemone","TubeWorm","VampireSquid","VelvetWorm","VenusComb",
+      "Whelk","Woodlouse","Zooplankton"
+    ]
   }
 };
 
